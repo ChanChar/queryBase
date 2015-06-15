0fb81fe9f36565631cd9d822383f80322959066d
Charles C. Lee <charleschanlee@gmail.com>
2015-06-15 11:45:44 -0700

    updated badge index and badge show css

--- app/assets/javascripts/models/badge.js
+++ app/assets/javascripts/models/badge.js
@@ -2,4 +2,21 @@ QueryBase.Models.Badge = Backbone.Model.extend({
 
   urlRoot: 'api/badges',
 
+  // parse: function (response) {
+  //   if (response.earned_badge) {
+  //     this.earned().set(response.earned_badge);
+  //     delete response.earned_badge;
+  //   }
+  //
+  //   return response;
+  // },
+  //
+  // earned: function () {
+  //   if (!this._earned_badge) {
+  //     this._earned_badge = new QueryBase.Models.Badge([], { badge: this });
+  //   }
+  //
+  //   return this._earned_badge;
+  // }
+
 });
--- app/views/api/badges/_badge.json.jbuilder
+++ app/views/api/badges/_badge.json.jbuilder
@@ -1,1 +1,7 @@
 json.extract! badge, :id, :title, :description, :image_url
+# 
+# if current_user.id == badge.user_id
+#   json.earned_badge do
+#     json.extract badge, :id
+#   end
+# end
