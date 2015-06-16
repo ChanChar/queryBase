8c83f71015198e3f4de38da62df5b855331b1b7e
Charles C. Lee <charleschanlee@gmail.com>
2015-06-15 17:48:50 -0700

    added questions search

--- app/assets/javascripts/views/questions/index.js
+++ app/assets/javascripts/views/questions/index.js
@@ -1,6 +1,6 @@
 QueryBase.Views.QuestionsIndex = Backbone.CompositeView.extend({
 
-  className: 'questions-index',
+  className: 'questions-index row',
   template: JST['questions/index'],
 
   initialize: function () {
@@ -9,9 +9,21 @@ QueryBase.Views.QuestionsIndex = Backbone.CompositeView.extend({
     this.collection.each(this.addQuestionView.bind(this));
   },
 
+  events: {
+    'keyup .questions-search': 'searchQuestions',
+  },
+
   render: function () {
     var questionIndexContent = this.template();
     this.$el.html(questionIndexContent);
+    this.attachSubviews();
+    return this;
+  },
+
+  renderSearchResults: function (questions) {
+    var questionsIndexContent = this.template();
+    this.collection.each(this.removeQuestionView.bind(this));
+    questions.forEach(this.addQuestionView.bind(this));
     this.attachSubviews();
     return this;
   },
@@ -23,6 +35,20 @@ QueryBase.Views.QuestionsIndex = Backbone.CompositeView.extend({
 
   removeQuestionView: function (question) {
     this.removeModelSubview('.question-index-items', question);
+  },
+
+  searchQuestions: function(event) {
+    var questionsIndexView = this;
+    _.debounce(questionsIndexView.filterQuestions(), 500);
+  },
+
+  filterQuestions: function () {
+    var searchParams = new RegExp($('.questions-search').val(), 'gi'); // g=global, i=case insensitive
+    var foundQuestions = this.collection.filter(function (question) {
+      return searchParams.test(question.get('title'));
+    });
+
+    this.renderSearchResults(foundQuestions);
   }
 
 });
